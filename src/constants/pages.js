51f2f2050fc3b8b04596c7eb814398ea9463ab83
ykotsiuba <jurok3x@gmail.com>
2024-05-09 15:01:30 +0300

    Added view page.

--- src/constants/pages.js
+++ src/constants/pages.js
@@ -1,3 +1,4 @@
-export const defaultPage = 'default';
+export const defaultPage = '';
+export const view = ':id';
 export const login = 'login';
 export const secretPage = 'secret';
--- src/constants/pagesURLs.js
+++ src/constants/pagesURLs.js
@@ -1,8 +1,9 @@
-import * as pages from './pages';
 import config from 'config';
+import * as pages from './pages';
 
 const result = {
   [pages.defaultPage]: `${config.UI_URL_PREFIX}/${pages.defaultPage}`,
+  [pages.view]: `${config.UI_URL_PREFIX}/${pages.view}`,
   [pages.login]: `${config.UI_URL_PREFIX}/${pages.login}`,
   [pages.secretPage]: `${config.UI_URL_PREFIX}/${pages.secretPage}`,
 };
