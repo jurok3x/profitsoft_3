0198830c23a5273fb94fad429a0dedbcb6a7b0b8
yurii kotsiuba <jurok3x@gmail.com>
2024-06-26 16:19:30 +0300

    commit

--- src/constants/pagesURLs.js
+++ src/constants/pagesURLs.js
@@ -4,7 +4,7 @@ import * as pages from './pages';
 const result = {
   [pages.defaultPage]: `${config.UI_URL_PREFIX}/${pages.defaultPage}`,
   [pages.view]: `${config.UI_URL_PREFIX}/${pages.view}`,
-  [pages.login]: `${config.UI_URL_PREFIX}/${pages.login}`,
+  [pages.login]: `https://httpwwwbookstore-yuriicom.com:8443/oauth/authenticate`,
   [pages.secretPage]: `${config.UI_URL_PREFIX}/${pages.secretPage}`,
 };
 
